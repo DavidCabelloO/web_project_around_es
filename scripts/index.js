8ee8c98197b44f51f94de6c54c91c1e021497f06
David <david.cabello.ortega.20@gmail.com>
2026-06-08 23:00:00 -0600

    setting profile edit modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,51 @@ initialCards = [
   },
 ];
 
+//Imprime en la consola todos los lugares del array initialCards
+/*
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+*/
+
+function openModal(modalEl) {
+  modalEl.classList.add("popup_is-opened");
+}
+
+function closeModal(modalEl) {
+  modalEl.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  openModal(popupModal);
+  fillProfileForm();
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(popupModal);
+}
+
+const profileSelect = document.querySelector(".profile"); //Sección del perfil
+const popupModal = document.querySelector("#edit-popup"); //Modal de edición del perfil
+
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileCloseEditBtn = popupModal.querySelector(".popup__close");
+
+const profileName = profileSelect.querySelector(".profile__title");
+const profileDescription = profileSelect.querySelector(".profile__description");
+
+let formElement = popupModal.querySelector("#edit-profile-form");
+let nameInput = formElement.querySelector(".popup__input_type_name");
+let jobInput = formElement.querySelector(".popup__input_type_description");
+
+profileEditBtn.addEventListener("click", () => handleOpenEditModal());
+formElement.addEventListener("submit", (evt) => handleProfileFormSubmit(evt));
+profileCloseEditBtn.addEventListener("click", () => closeModal(popupModal));
